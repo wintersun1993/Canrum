8f68fb5c677dfd6547b582004f5b3f33b95235cc
dimitar <dimitarstoyanov90@gmail.com>
2014-10-04 18:10:40 -0700

    Enemy Menu test function

--- Assets/Code/Enemy_Logic.cs
+++ Assets/Code/Enemy_Logic.cs
@@ -3,20 +3,31 @@ using System.Collections;
 
 public class Enemy_Logic : MonoBehaviour {
 	public GameObject rifle;
+	public bool clicked = false;
 	void OnTriggerEnter(Collider other) {
 		rifle.SetActive (true);
+
 	}
 	void OnTriggerExit (){
 		rifle.SetActive (false);
+
 	}
 	//onclick fight starts
 
-	public void OnMouseDown(){
-
-		print ("FIGHT");
+	public void OnGUI(){
+		if (clicked == true) {
+			GUI.Box(new Rect(10,10,100,100), "Menu");
+				}
 	}
-	void Behaviour(){
+	void Update(){
+		if (Input.GetMouseButtonDown(1)) {
+			clicked = true;
+			print ("ENEMY MENU");
+
+		}
+
 
 	}
+
 
 }
