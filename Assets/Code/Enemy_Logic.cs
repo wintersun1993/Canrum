965c957a252f2f77f729e66b25a13aaddf6dc0ef
dimitar <dimitarstoyanov90@gmail.com>
2014-10-06 05:23:24 -0700

    Audio on collision + menu

--- Assets/Code/Enemy_Logic.cs
+++ Assets/Code/Enemy_Logic.cs
@@ -4,22 +4,45 @@ using System.Collections;
 public class Enemy_Logic : MonoBehaviour {
 	public GameObject rifle;
 	public bool clicked = false;
+	public int seconds = 0;
+	public bool colliding = false;
+
+	//collision
 	void OnTriggerEnter(Collider other) {
 		rifle.SetActive (true);
+		colliding = true;
+		audio.Play();
 
 	}
+	//collision out
 	void OnTriggerExit (){
 		rifle.SetActive (false);
-
+		colliding = false;
+		seconds = 0;
+		audio.Stop();
 	}
 	//onclick fight starts
 
+	//show enemy menu
 	public void OnGUI(){
-		if (clicked == true) {
-			GUI.Box(new Rect(10,10,100,100), "Menu");
+		if (clicked == true || colliding == true) {
+			GUI.Box(new Rect(10,10,100,100), seconds.ToString());
 				}
 	}
 	void Update(){
+		//Hitpoints -- logic
+		if (colliding == true) {
+			seconds --;
+
+			if (seconds == -400) {
+
+			}
+
+		}
+
+		//hitpoints end logic
+
+		//Enemy menu loogic
 		if (Input.GetMouseButtonDown(1)) {
 			clicked = true;
 			print ("ENEMY MENU");
