1676b13450dba0e398bde5d7d5ad912d413d4dc2
Boyan Nedyalkov <wintersun1993@abv.bg>
2014-10-15 22:28:44 +0300

    Added code for main menu

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.gray;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+
+}
